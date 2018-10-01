59fdfeb0a0f2160f8faff65004088955082ee06c
vsparrow <khan@vsparrow.com>
2018-10-01 17:06:39 -0400

    initial message display for Display

--- src/component/DisplayMaster.js
+++ src/component/DisplayMaster.js
@@ -7,13 +7,20 @@ class DisplayMaster extends Component {
     }
   }
 
-  render() {
+  messageToViewerToSelectUser(){
+    console.log(`DisplayMaster: selectedUser is ${this.props.selectedUser}`);
+    if(this.props.selectedUser == null) {return <h1>Please select a user above</h1>}
+    //else call some subcomponent
+    else {return <h1>Get data for user {this.props.selectedUser} here</h1>}
+  }
 
+  render() {
     return (
       <div className="DisplayMaster">
         <h1>
             DisplayMaster
         </h1>
+        {this.messageToViewerToSelectUser()}
       </div>
     );
   }
